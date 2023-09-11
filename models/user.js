700a59af2f16891e7478ede635d31f9190fbf5e9
Abdul Basit <dev.basit2020@gmail.com>
2023-09-11 05:46:40 +0500

    User model refined

--- models/user.js
+++ models/user.js
@@ -20,10 +20,34 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 4,
+    minlength: 8,
     maxlength: 1024,
   },
-  isAdmin: Boolean,
+  phone: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 128,
+  },
+  address: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+  country: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 128,
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 128,
+  },
+  // isAdmin: Boolean,
 });
 
 userSchema.methods.generateAuthToken = function () {
@@ -45,7 +69,11 @@ function validateUser(user) {
   const schema = {
     name: Joi.string().min(2).max(50).required(),
     email: Joi.string().min(4).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(8).max(255).required(),
+    phone: Joi.string().min(4).max(128).required(),
+    address: Joi.string().min(5).max(1024).required(),
+    country: Joi.string().min(2).max(128).required(),
+    city: Joi.string().min(2).max(128).required(),
   };
 
   return Joi.validate(user, schema);
