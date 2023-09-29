e50cecdefa6788990f83dac2723e4e40fbe6690b
Abdul Basit <dev.basit2020@gmail.com>
2023-09-29 15:27:06 +0500

    Separation of concerns: CSS files added for screen and components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+import "./styles/components.css";
+import "./styles/screens.css";
 import App from "./App";
 import Toaster from "./Components/Toaster";
 
