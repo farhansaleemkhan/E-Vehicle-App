70aacb59356f130bc17cce52fbb7232ecbcd7671
Abdul Basit <dev.basit2020@gmail.com>
2023-09-29 15:34:28 +0500

    Added columns header

---
+++ src/constants/data/companies.js
@@ -0,0 +1,38 @@
+export const allCompaniesColumns = [
+  {
+    id: 1,
+    name: "Username",
+    value: "Username",
+    class: { width: "5rem", minWidth: "5rem" },
+  },
+  {
+    id: 2,
+    name: "Full Name",
+    value: "Full Name",
+    class: { width: "5rem", minWidth: "5rem" },
+  },
+  {
+    id: 3,
+    name: "Email",
+    value: "Email",
+    class: { width: "5rem", minWidth: "5rem" },
+  },
+  {
+    id: 4,
+    name: "Address",
+    value: "Address",
+    class: { width: "5rem", minWidth: "5rem" },
+  },
+  {
+    id: 5,
+    name: "City",
+    value: "City",
+    class: { width: "5rem", minWidth: "5rem" },
+  },
+  {
+    id: 6,
+    name: "Country",
+    value: "Country",
+    class: { width: "5rem", minWidth: "5rem" },
+  },
+];
---
+++ src/constants/data/departments.js
@@ -0,0 +1,14 @@
+export const allDepartmentsColumns = [
+  {
+    id: 1,
+    name: "Deaprtment",
+    value: "Deaprtment",
+    class: { width: "5rem", minWidth: "5rem" },
+  },
+  {
+    id: 1,
+    name: "Belongs To Company",
+    value: "Belongs To Company",
+    class: { width: "5rem", minWidth: "5rem" },
+  },
+];
