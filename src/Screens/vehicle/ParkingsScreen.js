e58e05c8a6c77720561ae7847a2fd8c1adf77b46
Abdul Basit <dev.basit2020@gmail.com>
2023-10-05 12:49:09 +0500

    Added tab bar

--- src/Screens/vehicle/ParkingsScreen.js
+++ src/Screens/vehicle/ParkingsScreen.js
@@ -8,17 +8,88 @@ import { parkingAreaService } from "../../services/vehicle/parkingAreaService";
 import { parkingAreasColumns, parkingColumns } from "../../constants/data/parkings";
 import { parkingService } from "../../services/vehicle/parkingService";
 import { AuthContext } from "../../context/AuthContext";
+import { Tabs } from "antd";
+
+const items = [
+  {
+    key: "1",
+    label: <h4>Current Parkings</h4>,
+    children: <CurrentParkings />,
+  },
+  {
+    key: "2",
+    label: <h4>Search Parking Area</h4>,
+    children: <SearchParkingArea />,
+  },
+  {
+    key: "3",
+    label: <h4>All Parking Areas</h4>,
+    children: <AllParkingAreas />,
+  },
+];
 
 export default function ParkingsScreen() {
-  const [allParkingAreas, setAllParkingAreas] = useState([]);
+  return (
+    <div className="mt-3 ml-3 mr-3">
+      <Tabs
+        defaultActiveKey="1"
+        items={items}
+        onChange={() => {}}
+        // tabBarStyle={{ background: "red", color: "white" }}
+      />
+    </div>
+  );
+}
+
+function CurrentParkings() {
   const [allParkings, setAllParkings] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    fetchAllParkings();
+  }, []);
+
+  const fetchAllParkings = async () => {
+    try {
+      const response = await parkingService.getParkings();
+
+      let tableBodyData = response.data.map((item) => ({
+        id: item._id,
+        parkingAreaId: item.parkingAreaId.name,
+        manufacturer: item.vehicleId.manufacturer,
+        model: item.vehicleId.model,
+        licensePlateNumber: item.vehicleId.licensePlateNumber,
+        chassisNumber: item.vehicleId.chassisNumber,
+        parkedTime: moment(+item.parkedTime).format("YYYY-MM-DD HH:mm:ss"),
+      }));
+
+      setAllParkings(tableBodyData);
+    } catch (error) {}
+  };
+
+  return (
+    <>
+      <div className="allCompaniesScreen">
+        {/* {currentUser.type === "admin" && ( */}
+        <DetailsContainer title="Parked Vehicles" showDropdown>
+          <div className="table-container">
+            <Table tableColumns={parkingColumns} tableBody={allParkings} />
+          </div>
+        </DetailsContainer>
+        {/* )} */}
+      </div>
+    </>
+  );
+}
+
+function SearchParkingArea() {
+  const [allParkingAreas, setAllParkingAreas] = useState([]);
   const [searchedParkingArea, setSearchedParkingArea] = useState([]);
   const [selectedParkingArea, setSelectedParkingArea] = useState("");
   const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     fetchAllParkingLocations();
-    fetchAllParkings();
   }, []);
 
   useEffect(() => {
@@ -59,60 +130,69 @@ export default function ParkingsScreen() {
     } catch (error) {}
   };
 
-  const fetchAllParkings = async () => {
+  return (
+    <>
+      <div className="allCompaniesScreen">
+        {/* {currentUser.type === "admin" && ( */}
+        <DetailsContainer title="Search Parking-Area by Name:" showDropdown>
+          <div style={{ margin: "2rem 0" }}>
+            <DropdownSearhable
+              idkey="id"
+              displayKey="name"
+              placeholder="Select Parking-Area."
+              // style={styles.dropDown.smallDropDownWithoutBorder}
+              list={allParkingAreas}
+              selectedItem={selectedParkingArea}
+              setSelectedItem={setSelectedParkingArea}
+            ></DropdownSearhable>
+          </div>
+
+          <div className="table-container">
+            <Table tableColumns={parkingAreasColumns} tableBody={searchedParkingArea} />
+          </div>
+        </DetailsContainer>
+
+        {/* )} */}
+      </div>
+    </>
+  );
+}
+
+function AllParkingAreas() {
+  const [allParkingAreas, setAllParkingAreas] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    fetchAllParkingLocations();
+  }, []);
+
+  const fetchAllParkingLocations = async () => {
     try {
-      const response = await parkingService.getParkings();
+      const response = await parkingAreaService.getParkingAreas();
 
       let tableBodyData = response.data.map((item) => ({
         id: item._id,
-        parkingAreaId: item.parkingAreaId.name,
-        manufacturer: item.vehicleId.manufacturer,
-        model: item.vehicleId.model,
-        licensePlateNumber: item.vehicleId.licensePlateNumber,
-        chassisNumber: item.vehicleId.chassisNumber,
-        parkedTime: moment(+item.parkedTime).format("YYYY-MM-DD HH:mm:ss"),
+        name: item.name,
+        totalSlots: item.totalSlots,
+        bookedSlots: item.bookedSlots,
+        location: "VIEW",
+        belongsTo: item.belongsTo.userId.username,
       }));
 
-      setAllParkings(tableBodyData);
+      setAllParkingAreas(tableBodyData);
     } catch (error) {}
   };
 
   return (
     <>
       <div className="allCompaniesScreen">
-        {currentUser.type === "admin" && (
-          <>
-            <DetailsContainer title="Search Parking-Area by Name:" showDropdown>
-              <div style={{ margin: "2rem 0" }}>
-                <DropdownSearhable
-                  idkey="id"
-                  displayKey="name"
-                  placeholder="Select Parking-Area."
-                  // style={styles.dropDown.smallDropDownWithoutBorder}
-                  list={allParkingAreas}
-                  selectedItem={selectedParkingArea}
-                  setSelectedItem={setSelectedParkingArea}
-                ></DropdownSearhable>
-              </div>
-
-              <div className="table-container">
-                <Table tableColumns={parkingAreasColumns} tableBody={searchedParkingArea} />
-              </div>
-            </DetailsContainer>
-
-            <DetailsContainer title="All Parking-Areas:" showDropdown>
-              <div className="table-container">
-                <Table tableColumns={parkingAreasColumns} tableBody={allParkingAreas} />
-              </div>
-            </DetailsContainer>
-
-            <DetailsContainer title="All Parkings (Vehicles Parked):" showDropdown>
-              <div className="table-container">
-                <Table tableColumns={parkingColumns} tableBody={allParkings} />
-              </div>
-            </DetailsContainer>
-          </>
-        )}
+        {/* {currentUser.type === "admin" && ( */}
+        <DetailsContainer title="All Parking-Areas:" showDropdown>
+          <div className="table-container">
+            <Table tableColumns={parkingAreasColumns} tableBody={allParkingAreas} />
+          </div>
+        </DetailsContainer>
+        {/* )} */}
       </div>
     </>
   );
