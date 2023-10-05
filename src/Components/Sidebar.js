bb0757ae647aba647e97c53853a0b8aaaed8cef3
Abdul Basit <dev.basit2020@gmail.com>
2023-10-05 16:49:30 +0500

    Added role base

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,24 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-const sidebarItems = [
-  { id: 2, name: "Departments", link: "/departments" },
-  { id: 3, name: "Employees", link: "/employees" },
-  { id: 4, name: "Vehicles", link: "/vehicles" },
-  { id: 5, name: "Parking Areas", link: "/parkings" },
-  { id: 2, name: "X=Departments=X", link: "/register-company" },
-  { id: 3, name: "X=Employees=X", link: "/book" },
-  { id: 4, name: "X=Vehicles=X", link: "/home" },
-  { id: 5, name: "X=Parking Areas=X", link: "/company" },
-];
-
-const userType = "employee";
-
-userType === "employee"
-  ? sidebarItems.unshift({ id: 1, name: "Companies", link: "/companies" })
-  : sidebarItems.unshift({ id: 1, name: "Company", link: "/companies" });
+import { getLocalStorageItem } from "../utils/localStorage";
 
 const Sidebar = () => {
+  let sidebarItems = null;
+  const userType = getLocalStorageItem("userType");
+
+  if (userType === "admin") {
+    sidebarItems = [
+      { id: 1, name: "Companies", link: "/companies" },
+      { id: 2, name: "Departments", link: "/departments" },
+      { id: 3, name: "Employees", link: "/employees" },
+      { id: 4, name: "Vehicles", link: "/vehicles" },
+      { id: 5, name: "Parking Areas", link: "/parkings" },
+      { id: 2, name: "X=Departments=X", link: "/register-company" },
+      { id: 3, name: "X=Employees=X", link: "/book" },
+      { id: 4, name: "X=Vehicles=X", link: "/home" },
+      { id: 5, name: "X=Parking Areas=X", link: "/company" },
+    ];
+  } else if (userType === "company") {
+    sidebarItems = [
+      { id: 1, name: "Company", link: "/companies" },
+      { id: 2, name: "Departments", link: "/departments" },
+      { id: 3, name: "Employees", link: "/employees" },
+      { id: 4, name: "Vehicles", link: "/vehicles" },
+      { id: 5, name: "Parking Areas", link: "/parkings" },
+    ];
+  } else if (userType === "employee") {
+    sidebarItems = [
+      { id: 1, name: "Vehicles", link: "/vehicles" },
+      { id: 2, name: "Parking Areas", link: "/parkings" },
+    ];
+  }
+
   // return <div className="sidebar"></div>;
   return (
     <div className="sidebar">
