cccf8ee2e9e4332eb8838cce044caa089b8c45fe
Abdul Basit <dev.basit2020@gmail.com>
2023-11-19 14:55:23 +0500

    Added css for active sidebar tab

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { getLocalStorageItem } from "../utils/localStorage";
 
 const Sidebar = () => {
   let sidebarItems = null;
   const userType = getLocalStorageItem("userType");
   const companyId = getLocalStorageItem("companyId");
+  const location = useLocation();
 
   if (userType === "admin") {
     sidebarItems = [
@@ -14,10 +15,6 @@ const Sidebar = () => {
       { id: 1, name: "Companies", link: "/companies" },
       { id: 2, name: "Departments", link: "/departments" },
       { id: 3, name: "Employees", link: "/employees" },
-      // { id: 2, name: "X=Departments=X", link: "/register-company" },
-      // { id: 3, name: "X=Employees=X", link: "/book" },
-      // { id: 4, name: "X=Vehicles=X", link: "/home" },
-      // { id: 5, name: "X=Parking=X", link: "/company" },
     ];
   } else if (userType === "company") {
     sidebarItems = [
@@ -26,31 +23,22 @@ const Sidebar = () => {
       { id: 1, name: "Company", link: `/company/details?id=${companyId}` },
       { id: 2, name: "Departments", link: "/departments" },
       { id: 3, name: "Employees", link: "/employees" },
-      // { id: 2, name: "X=Departments=X", link: "/register-company" },
-      // { id: 3, name: "X=Employees=X", link: "/book" },
-      // { id: 4, name: "X=Vehicles=X", link: "/home" },
-      // { id: 5, name: "X=Parking=X", link: "/company" },
     ];
   } else if (userType === "employee") {
-    sidebarItems = [
-      { id: 2, name: "Parking", link: "/parkings" },
-      // { id: 1, name: "Vehicles", link: "/vehicles" },
-    ];
+    sidebarItems = [{ id: 2, name: "Parking", link: "/parkings" }];
   }
 
-  // return <div className="sidebar"></div>;
   return (
     <div className="sidebar">
-      <ul className="">
+      <ul>
         {sidebarItems.map((item) => {
+          const isActive = location.pathname === item.link;
+
           return (
-            <li className="nav-item" key={item.id}>
-              <Link className="nav-link active" to={item.link}>
+            <li key={item.id}>
+              <Link className={`nav-link ${isActive ? "active" : ""}`} to={item.link}>
                 {item.name}
               </Link>
-              {/* <a className="nav-link active" href={item.link}>
-                {item.name}
-              </a> */}
             </li>
           );
         })}
